31289fae976717d7a88e73da3e6be231b239a13d
Allan Callaghan <calaldees@gmail.com>
2022-12-22 17:05:25 +0000

    Added sprite data from dissasembly sprite data is currently in hex - this will be replaced with base64 soon Added colors object

--- image_decode.js
+++ image_decode.js
@@ -1,5 +1,5 @@
 const nonHexRegex = new RegExp('[^0-9a-f]+', 'gi')
-
+function cleanHexString(hex_string) {return hex_string.toLowerCase().replaceAll(nonHexRegex, "")}
 function* hexToBytes(hex) {
     console.assert(hex.length % 2 == 0)
     for (let c = 0; c < hex.length; c += 2) {
@@ -40,23 +40,34 @@ function ImageDataToImageBitmap(imageDataArray) {
         c.putImageData(imageDataArray[1], 8, 0)
         c.putImageData(imageDataArray[2], 0, 8)
         c.putImageData(imageDataArray[3], 8, 8)
-    } else 
-    // TODO: length 12? for 3 frame animation?
-    {
+    } else {
         throw `unable to process imageDataArray`
     }
     return offscreen.transferToImageBitmap()
 }
 
-function loadImage(data) {
+const BASE64_HEAD = "data:application/octet;base64,"
+function base64ToUint8Array(base64_string) {
+    base64_string = base64_string.startsWith(BASE64_HEAD) ? base64_string.substr(BASE64_HEAD.length) : base64_string
+    return Uint8Array.from(atob(base64_string), c => c.charCodeAt(0))  // https://stackoverflow.com/a/41106346/3356840
+    // await (await fetch("data:application/octet;base64," + base64data)).arrayBuffer()
+    // https://stackoverflow.com/questions/21797299/convert-base64-string-to-arraybuffer#comment124033543_49273187
+}
+
+export function loadImage(data) {
     if (typeof(data) == "string" && data.startsWith("data:")) {throw "do some base64 decoding shiz"}
-    if (typeof(data) == "string") {data = hexToBytes(data.toLowerCase().replaceAll(nonHexRegex, ""))}
+    if (typeof(data) == "string") {data = hexToBytes(cleanHexString(data))}
     return ImageDataToImageBitmap([...bytesToMono8x8ImageDataChunks(data)])
 }
-
-// await (await fetch("data:application/octet;base64," + base64data)).arrayBuffer()
-// https://stackoverflow.com/questions/21797299/convert-base64-string-to-arraybuffer#comment124033543_49273187
-
-export {
-    loadImage,
+export function* loadImages(data) {
+    if (typeof(data) == "string") {data = hexToBytes(cleanHexString(data))}
+    const chunks = []
+    for (let Mono8x8ImageDataChunk of bytesToMono8x8ImageDataChunks(data)) {
+        chunks.push(Mono8x8ImageDataChunk)
+        if (chunks.length==4) {
+            yield ImageDataToImageBitmap(chunks)
+            chunks.length = 0
+        }
+    }
 }
+
--- image_draw.js
+++ image_draw.js
@@ -1,18 +1,17 @@
 // https://en.wikipedia.org/wiki/ZX_Spectrum_graphic_modes#Colour_palette
-const COLOR = {
-    /*
-    0 	0 	0 	0 	0 	#000 	Black 	0 	0 	0 	1 	#000 	Black
-    1 	0 	0 	1 	0 	#00E 	Blue 	0 	0 	1 	1 	#00F 	Bright Blue
-    2 	0 	1 	0 	0 	#E0 	Red 	0 	1 	0 	1 	#F00 	Bright Red
-    3 	0 	1 	1 	0 	#E0E 	Magenta 	0 	1 	1 	1 	#F0F 	Bright Magenta
-    4 	1 	0 	0 	0 	#0E0 	Green 	1 	0 	0 	1 	#0F0 	Bright Green
-    5 	1 	0 	1 	0 	#0EE 	Cyan 	1 	0 	1 	1 	#0FF 	Bright Cyan
-    6 	1 	1 	0 	0 	#EE0 	Yellow 	1 	1 	0 	1 	#FF0 	Bright Yellow
-    7 	1 	1 	1 	0 	#EEE 	White 	1 	1 	1 	1 	#FFF 	Bright White
-    */
+
+export const COLOR = {
+    black: "#000",
+    blue: "#00E",
+    red: "#E00",
+    magenta: "#E0E",
+    green: "#0E0",
+    cyan: "#0EE",
+    yellow: "#EE0",
+    white: "#EEE",
 }
 
-function shiftImage(imageBitmap, color) {
+export function shiftImage(imageBitmap, color) {
     const w = imageBitmap.width
     const h = imageBitmap.height
     const o = new OffscreenCanvas(w, h)
@@ -24,8 +23,4 @@ function shiftImage(imageBitmap, color) {
     c.fillRect(0,0,w,h)
     
     return o.transferToImageBitmap()
-}
-
-export {
-    shiftImage,
 }
