054e71903d923a65479964d9d0aa4ffc37b48fac
Allan Callaghan <allan@calaldees.uk>
2024-07-24 08:02:56 +0100

    First gfx_effects playing

--- gfx/effects.js
+++ gfx/effects.js
@@ -1,20 +1,32 @@
 import { filterInPlace, Dimension } from "../core.js"
 
 // These are duplicated in 'map' and 'map_model' - I am sad at this
-const BORDER_OFFSET = 8  // Probably needs to be passed rather than a constant? Maybe the context can be transposed?
+const BORDER_OFFSET_PX = 8  // Probably needs to be passed rather than a constant? Maybe the context can be transposed?
 const CELL_SIZE_PX = 16
 const dimension = new Dimension(15, 10)
 
 export class _GfxEffect {
-    get hasExpired() {return false}
+    get active() {return true}
+}
+
+function i_to_xy(i) {
+    return [...dimension.index_to_position(i)].map(j=>Math.floor(j*CELL_SIZE_PX)+BORDER_OFFSET_PX)
 }
 
 export class SpriteEffect extends _GfxEffect {
-    constructor(sprites) {
+    constructor(sprites, i) {
+        super(...arguments)
         this.sprites = sprites
+        this.i = i
     }
 
-    draw(c, frame) {}
+    draw(c, frame) {
+        if (frame%8 != 0) {return}
+        const f = Math.floor(frame/8)%this.sprites.length
+        const [_x, _y] = i_to_xy(this.i)
+        c.clearRect(_x,_y,16,16)
+        c.drawImage(this.sprites[f], _x, _y)
+    }
 }
 
 export class HighlightEffect extends _GfxEffect {
@@ -40,6 +52,6 @@ export class GfxEffects {
         for (let effect of this.effects) {
             effect.draw(c, frame)
         }
-        filterInPlace(this.effects, (effect)=>effect.hasExpired, this)
+        filterInPlace(this.effects, (effect)=>effect.active, this)
     }
 }
