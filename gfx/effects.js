97cbd32ecec833664cb78d83b7df523537f0644d
Allan Callaghan <allan@calaldees.uk>
2024-07-25 08:11:53 +0100

    Attempt at highliting effect This is not going to work because cells are persistant. I need to consider layers

--- gfx/effects.js
+++ gfx/effects.js
@@ -4,33 +4,48 @@ import { filterInPlace, Dimension } from "../core.js"
 const BORDER_OFFSET_PX = 8  // Probably needs to be passed rather than a constant? Maybe the context can be transposed?
 const CELL_SIZE_PX = 16
 const dimension = new Dimension(15, 10)
+function i_to_xy(i) {
+    return [...dimension.index_to_position(i)].map(j=>Math.floor(j*CELL_SIZE_PX)+BORDER_OFFSET_PX)
+}
+
 
 export class _GfxEffect {
     get active() {return true}
 }
 
-function i_to_xy(i) {
-    return [...dimension.index_to_position(i)].map(j=>Math.floor(j*CELL_SIZE_PX)+BORDER_OFFSET_PX)
-}
-
 export class SpriteEffect extends _GfxEffect {
     constructor(sprites, i) {
-        super(...arguments)
+        super()  //...arguments
         this.sprites = sprites
         this.i = i
     }
-
     draw(c, frame) {
-        if (frame%8 != 0) {return}
-        const f = Math.floor(frame/8)%this.sprites.length
+        if (frame % 16 != 0) {return}
+        const f = Math.floor(frame/16)%this.sprites.length
         const [_x, _y] = i_to_xy(this.i)
-        c.clearRect(_x,_y,16,16)
+        c.clearRect(_x,_y,CELL_SIZE_PX,CELL_SIZE_PX)
         c.drawImage(this.sprites[f], _x, _y)
     }
 }
 
 export class HighlightEffect extends _GfxEffect {
-    draw(c, frame) {}
+    constructor(color, indexs, alpha=0.4, frame_duration=16, frame_alpha_multiplier=0.05) {
+        super()
+        this.color = color
+        this.indexs = indexs
+        this.alpha = alpha
+        this.frame_duration = frame_duration
+        this.frame_alpha_multiplier = frame_alpha_multiplier
+    }
+    draw(c, frame) {
+        c.save()
+        c.globalAlpha = this.alpha //+ ((frame % this.frame_duration)-(this.frame_duration/2))*this.frame_alpha_multiplier
+        for (let i of this.indexs) {
+            c.fillStyle = this.color
+            c.fillRect(...i_to_xy(i), CELL_SIZE_PX, CELL_SIZE_PX)
+        }
+        c.restore()
+    }
 }
 
 export class VectorEffect extends _GfxEffect {
