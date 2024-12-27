61b95b19e92db1a6c943afa50b0fa3bc3fdb9fb2
Allan Callaghan <allan@calaldees.uk>
2024-12-27 11:59:20 +0000

    Close approximation of original character select screen

--- src/ui/character_select.js
+++ src/ui/character_select.js
@@ -3,6 +3,7 @@ import { COLOR, shiftImage } from '../gfx/color.js'
 
 import {gfx_units} from '../gfx/units.js'
 import {mergeItemsAndLayout} from './ui_canvas.js'
+import { FONT_HEIGHT } from '../gfx/text.js'
 
 export class UICharacterSelect {
     constructor(ui) {
@@ -28,20 +29,40 @@ export class UICharacterSelect {
             {action:"Wizard MERLIN", text:'  ', color: COLOR.cyan},
             {action:"Wizard ILIAN RANE", text:'  ', color: COLOR.cyan},
             {action:"Wizard ASIMONO ZARK", text:'  ', color: COLOR.cyan},
-        ], this.UI_INDEXES_ORIGINAL_CHAR_SELECT())]
 
-        for (let item of ui.items) {
+            {action:"Color red_bright", text:'  ', color: COLOR.cyan},
+            {action:"Color magenta_bright", text:'  ', color: COLOR.cyan},
+            {action:"Color green_bright", text:'  ', color: COLOR.cyan},
+            {action:"Color cyan_bright", text:'  ', color: COLOR.cyan},
+            {action:"Color yellow", text:'  ', color: COLOR.cyan},
+            {action:"Color yellow_bright", text:'  ', color: COLOR.cyan},
+            {action:"Color white", text:'  ', color: COLOR.cyan},
+            {action:"Color white_bright", text:'  ', color: COLOR.cyan},
+        ], [...this.UI_INDEXES_CHAR_SELECT(), ...this.UI_INDEXES_COLOR()])]
+
+        for (let item of ui.items.slice(0,8)) {
             this.ui.c.drawImage(
                 shiftImage(...gfx_units[item.action].sprite_and_color(0)),
                 ...this.ui.i_to_xy(item.i + 1)
             )
         }
+        for (let item of ui.items.slice(8,16)) {
+            this.ui.c.fillStyle = COLOR[item.action.replace('Color ', '')]
+            this.ui.c.fillRect(...this.ui.i_to_xy(item.i + 1),FONT_HEIGHT,FONT_HEIGHT)
+        }
     }
 
-    UI_INDEXES_ORIGINAL_CHAR_SELECT(start_row=6) {
+    UI_INDEXES_CHAR_SELECT(start_row=6) {
         return [...range(8)].map((r)=>{return {
             'i': (this.ui.dimension.width*start_row)+(r*3),
             'key': String.fromCharCode(r+49),
+        }})
+    }
+
+    UI_INDEXES_COLOR(start_row=8) {
+        return [...range(8)].map((r)=>{return {
+            'i': (this.ui.dimension.width*start_row)+(r*3),
+            'key': String.fromCharCode(r+65),
         }})
     }
 
